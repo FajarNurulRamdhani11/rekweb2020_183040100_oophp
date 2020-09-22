2560745ad833e4e869deaeb1e0033ef4cdb579e2
Fajar Nurul Ramdhani <71371240+FajarNurulRamdhani11@users.noreply.github.com>
2020-09-22 16:26:23 +0700

    Add files via upload

---
+++ produk.php
@@ -0,0 +1,46 @@
+<?php 
+
+
+Class Produk {
+	public 	$judul = "judul",
+			$penulis = "penulis",
+			$penerbit = "penerbit",
+			$harga = 0;
+
+
+	public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0 ) {
+		$this->pjudul = $judul;
+		$this->penulis = $penulis;
+		$this->penerbit = $penerbit;
+		$this->harga = $harga;
+	}
+
+
+	public function getLabel() {
+		return "$this->penulis, $this->penerbit";
+	}
+
+	
+	
+
+
+}
+
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
+
+// $produk2 = new Produk();
+// $produk2->judul = "Uncharted";
+// $produk2->tambahProperty = "hahaha";
+// var_dump($produk2); 
+
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Masashi Kishimoto", "Masashi Kishimoto", "Shonen Jump",  30000 );
+
+$produk2 = new Produk("Uncharted","Neil Duckmann", " Sony Computer", 250000);
+
+$produk3 = new Produk("Dragon Ball");
+
+echo "Komik : " . $produk1->getLabel();
+echo "<br>";
+echo "Game   : " . $produk2->getLabel();
